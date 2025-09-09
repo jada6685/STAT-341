12c8195a3780e2a9e39998afd63d11452c111698
Jada <jfickling@claflin.edu>
2025-09-09 16:13:27 -0400

    Add files via upload

---
+++ Correlation Type Test.R
@@ -0,0 +1,8 @@
+#Author: Jada Fickling, Date: 9/4/2025, Purpose Test the Correlation
+
+if(!require(devtools)) install.packages("devtools");
+devtools::install_github("kassambara/ggpubr")
+
+#Load the library ggpubr
+library(ggpubr)
+
